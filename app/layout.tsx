b42b352d2a1ee1b86ed31816b5f423aabf278934
Aditya Sharma <adityasharma2005123@gmail.com>
2025-02-20 16:26:30 +0530

    Added all the features and files

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { Work_Sans } from "next/font/google";
+
+import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+import Room from "./Room";
+
+export const metadata = {
+  title: "Amber",
+  description: "Where Ideas Ignite & Designs Shine",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  weight: ["400", "600", "700"],
+});
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang='en'>
+    <body className={`${workSans.className} bg-primary-grey-200`}>
+      <Room>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Room>
+    </body>
+  </html>
+);
+
+export default RootLayout;
